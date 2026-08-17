fa39a9273683fdb84bd591c6717a5be4eba24b82
mahdi <mahdi@1jz.cc>
2026-08-17 16:27:50 +0300

    add: nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+          ];
+
+          shellHook = ''
+            echo "watchy watchy!"
+            go version
+          '';
+        };
+      }
+    );
+}
