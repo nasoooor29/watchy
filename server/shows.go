1077645d1e46b8c59e8233f77bb462aa679c37c3
nasoooor <nasoooor29@gmail.com>
2026-08-22 15:32:38 +0300

    remove: GetShowSeason function

--- server/shows.go
+++ server/shows.go
@@ -108,29 +108,6 @@ func (s *Server) GetShow(w http.ResponseWriter, r *http.Request) {
 	s.render(w, r, http.StatusOK, "home.html", page)
 }
 
-func (s *Server) GetShowSeason(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
-	if err != nil || id < 0 {
-		http.NotFound(w, r)
-		return
-	}
-
-	seasonIndex, err := strconv.Atoi(r.PathValue("season"))
-	if err != nil || seasonIndex < 0 {
-		http.NotFound(w, r)
-		return
-	}
-
-	detail, err := buildShowDetail(s.DB, id)
-	if err != nil {
-		slog.Error("failed to build show season", "id", id, "season", seasonIndex, "err", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	s.render(w, r, http.StatusOK, "season.html", detail)
-}
-
 func (s *Server) GetPoster(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
 	if err != nil || id < 0 {
